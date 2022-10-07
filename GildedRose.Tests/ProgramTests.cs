611e0da7e66753e5fdeef8b6cc96e65b597bcf4a
Magnus Pedersen <magnus4p@gmail.com>
2022-10-07 14:51:48 +0200

    First attempt with edited item class (Illegal)
    Co-authored-by: bhagelin1999 <bhagelin1999@users.noreply.github.com>

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -1,10 +1,229 @@
 namespace GildedRose.Tests;
 
 public class ProgramTests
-{
+{   
+    
     [Fact]
     public void TestTheTruth()
     {
         true.Should().BeTrue();
+    }
+
+    [Fact]
+    public void AgedBrieIncreaseQualityDecreaseSellIn()
+    {
+        // Given
+        Cheese agedBrie = new Cheese { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+        IList<Item> items = new List<Item>{agedBrie};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Aged Brie", SellIn = 1, Quality = 1 });
+    }
+
+    [Fact]
+    public void SulfurasNeverChange()
+    {
+        // Given
+        LegendaryItem sulfur1 = new LegendaryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 };
+        LegendaryItem sulfur2 = new LegendaryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = 6, Quality = 80 };
+        LegendaryItem sulfur3 = new LegendaryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = -6, Quality = 80 };
+        IList<Item> items = new List<Item>{sulfur1, sulfur2, sulfur3};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+        program.Items[1].Should().BeEquivalentTo(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 6, Quality = 80 });
+        program.Items[2].Should().BeEquivalentTo(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -6, Quality = 80 });
+    }
+
+    [Fact]
+    public void ElixirDecreaseQualityDecreaseSellIn()
+    {
+        // Given
+        NormalItem exlixir = new NormalItem { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 };
+        IList<Item> items = new List<Item>{exlixir};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Elixir of the Mongoose", SellIn = 4, Quality = 6 });
+    }
+
+    [Fact]
+    public void BackstagePassNormalSellInAndNormalQualityWhenAbove10DaysSellin()
+    {
+        // Given
+        ConcertTicket BSP1 = new ConcertTicket
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                };
+        IList<Item> items = new List<Item>{BSP1};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 14, Quality = 21 });
+    }
+
+    [Fact]
+    public void BackstagePassNormalSellInAndDoubleQualityWhenBelow10DaysSellin()
+    {
+        // Given
+        ConcertTicket BSP1 = new ConcertTicket
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 9,
+                    Quality = 20
+                };
+        IList<Item> items = new List<Item>{BSP1};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 8, Quality = 22 });
+    }
+
+    [Fact]
+    public void BackstagePassQualityIncreasesBy3WhenSellInBetween0And5()
+    {
+        // Given
+        ConcertTicket BSP1 = new ConcertTicket
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 20
+                };
+        IList<Item> items = new List<Item>{BSP1};
+        Program program = new Program() { Items = items };
+    
+        // When
+        
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 23 });
+    }
+
+    [Fact]
+    public void BackstagePassQuality0WhenSellin0()
+    {
+        // Given
+        ConcertTicket BSP1 = new ConcertTicket
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 0,
+                    Quality = 20
+                };
+        IList<Item> items = new List<Item>{BSP1};
+        Program program = new Program() { Items = items };
+    
+        // When
+        
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 0 });
+    }
+
+    [Fact]
+    public void ElixirDecreaseQualityTwiceWhenNegativeSellin()
+    {
+        // Given
+        NormalItem exlixir = new NormalItem { Name = "Elixir of the Mongoose", SellIn = -1, Quality = 7 };
+        IList<Item> items = new List<Item>{exlixir};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Elixir of the Mongoose", SellIn = -2, Quality = 5 });
+    }
+
+    [Fact]
+    public void ElixirQualityNotBelow0()
+    {
+        // Given
+        NormalItem exlixir = new NormalItem { Name = "Elixir of the Mongoose", SellIn = -1, Quality = 1 };
+        IList<Item> items = new List<Item>{exlixir};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Elixir of the Mongoose", SellIn = -2, Quality = 0 });
+    }
+
+    [Fact]
+    public void AgedBrieQualityCapAt50()
+    {
+        // Given
+        Cheese agedBrie = new Cheese { Name = "Aged Brie", SellIn = -2, Quality = 50 };
+        IList<Item> items = new List<Item>{agedBrie};
+        Program program = new Program() { Items = items };
+    
+        // When
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Aged Brie", SellIn = -3, Quality = 50 });
+    }
+
+    [Fact]
+    public void BackstagePassQualityStay0WhenNegative0()
+    {
+        // Given
+        ConcertTicket BSP1 = new ConcertTicket
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = -5,
+                    Quality = 0
+                };
+        IList<Item> items = new List<Item>{BSP1};
+        Program program = new Program() { Items = items };
+    
+        // When
+        
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -6, Quality = 0 });
+    }
+
+    [Fact]
+    public void BackstagePassQualityStay50()
+    {
+        // Given
+        ConcertTicket BSP1 = new ConcertTicket
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 2,
+                    Quality = 50
+                };
+        IList<Item> items = new List<Item>{BSP1};
+        Program program = new Program() { Items = items };
+    
+        // When
+        
+        program.UpdateQuality();
+    
+        // Then
+        program.Items[0].Should().BeEquivalentTo(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 50 });
     }
 }
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -3,136 +3,22 @@ using System.Collections.Generic;
 
 namespace GildedRose
 {
-    class Program
+    public class Program
     {
-        IList<Item> Items;
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("OMGHAI!");
-
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
-                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-				// this conjured item does not work properly yet
-				new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
-                                          }
-
-                          };
-
-            for (var i = 0; i < 31; i++)
-            {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < app.Items.Count; j++)
-                {
-                    Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
-                }
-                Console.WriteLine("");
-                app.UpdateQuality();
-            }
-
-        }
-
+        public IList<Item> Items;
+        static void Main(string[] args){} //OneLine Main
+           
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            for (var i = 0; i < Items.Count; i++) {
+                //Items[i] => Type Of "LegendaryItem" or something like this
+                Items[i].UpdateItemQuality();
+                if(Items[i].Quality < 0)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    Items[i].Quality = 0;
                 }
             }
         }
-
     }
 
     public class Item
@@ -142,6 +28,81 @@ namespace GildedRose
         public int SellIn { get; set; }
 
         public int Quality { get; set; }
+
+        public virtual void UpdateItemQuality() {}
+        
+    }
+
+    public class LegendaryItem : Item
+    {
+        //We want to set Quality to always be 80 on all Legendary Items but right now it is just done when creating the item (in tests)
+    }
+
+    public class NormalItem : Item
+    {
+        public bool isConjured { get; set; }
+    
+        public override void UpdateItemQuality(){
+            SellIn = SellIn - 1;
+            if (isConjured || SellIn < 0) 
+            {
+                if (isConjured && SellIn < 0)
+                {
+                    Quality -= 4;
+                }
+                else 
+                {
+                    Quality -= 2;
+                }
+                
+            } 
+            else 
+            {  
+                Quality -= 1;
+            }
+        }
+    }
+
+
+    public class Cheese : Item
+    {
+        public override void UpdateItemQuality()
+        {
+            if(Quality < 50)
+            {
+                Quality += 1;
+            }
+            SellIn -= 1;
+        }
+    }
+
+    public class ConcertTicket : Item
+    {
+        public override void UpdateItemQuality()
+        {
+            SellIn -= 1;
+            
+            if (SellIn > 10)
+            {
+                Quality += 1;
+            }
+            else if (SellIn > 5)
+            {
+                Quality += 2;
+            }
+            else if (SellIn >= 0)
+            {
+                Quality += 3;
+            }
+            else 
+            {
+                Quality = 0;
+            }
+         
+            
+            if (Quality > 50) Quality = 50;
+            
+        }
     }
 
 }
